0379bebb66c4965a4404e1aab6ef45d5a8d5622f
alexander-morozov-travelline <morozov.alexvitvvv@gmail.com>
2021-01-22 03:03:57 +0300

    Доработка верстки

--- source/js/main.js
+++ source/js/main.js
@@ -22,3 +22,6 @@ function init () {
 
   map.geoObjects.add(placemark);
 }
+
+/* background-image - webp */
+var i=new Image;i.onload=i.onerror=function(){document.body.classList.add(i.height==1?"webp":"no-webp")};i.src="data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==";
